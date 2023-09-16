42b6f52a65a98b6f7ada3d77ad712960107f4691
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-16 17:25:33 +0300

    Implemented the task filtering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList, TaskType} from "./components/TodoList/TodoList";
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Tasks} from "../Tasks/Tasks";
 
 type TodolistPropsType = {
     title: string
@@ -13,7 +14,24 @@ export type TaskType = {
 
 export const TodoList: React.FC<TodolistPropsType> = ({title, tasks}: TodolistPropsType) => {
 
-    // const {title, tasks} = props
+    const [filter, setFilter] = useState('all');
+    let filteredTasks: Array<TaskType>;
+
+    switch (filter) {
+        case 'active':
+            filteredTasks = tasks.filter((task) => !task.isDone);
+            break;
+        case 'completed':
+            filteredTasks = tasks.filter((task) => task.isDone);
+            break;
+        default:
+            filteredTasks = tasks;
+    }
+
+    const onFilteredTasks = (filter: string) => {
+        setFilter(filter);
+    }
+
     return (
         <div className="todolist">
             <h3>{title}</h3>
@@ -21,25 +39,12 @@ export const TodoList: React.FC<TodolistPropsType> = ({title, tasks}: TodolistPr
                 <input/>
                 <button>+</button>
             </div>
-            <ul>
-                {
-                    tasks.map(item => {
-                        return (
-                            <li key={item.id}>
-                                <input type="checkbox" checked={item.isDone}/>
-                                <span>{item.title}</span>
-                            </li>
-                        )
-                    })
-                }
-                {/*<li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>*/}
-                {/*<li><input type="checkbox" checked={true}/> <span>JS</span></li>*/}
-                {/*<li><input type="checkbox" checked={false}/> <span>React</span></li>*/}
-            </ul>
+            <Tasks tasks={filteredTasks}/>
+
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => {onFilteredTasks('all')}}>All</button>
+                <button onClick={() => {onFilteredTasks('active')}}>Active</button>
+                <button onClick={() => {onFilteredTasks('completed')}}>Completed</button>
             </div>
         </div>
     );
