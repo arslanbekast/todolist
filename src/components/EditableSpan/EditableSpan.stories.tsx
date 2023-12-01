bafd134660bef9432d7fbfeca28408745bd6c310
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-01 11:21:38 +0300

    Created EditableSpan stories

---
+++ src/components/EditableSpan/EditableSpan.stories.tsx
@@ -0,0 +1,42 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {Button, TextField} from "@mui/material";
+import {EditableSpan} from "./EditableSpan";
+
+// More on how to set up stories at:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof EditableSpan> = {
+    title: 'TODOLISTS/EditableSpan',
+    component: EditableSpan,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry:
+    // https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes:
+    // https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        onClick: {
+            description: 'Clicked change span',
+            action: 'Clicked'
+        },
+        title: {
+            description: 'Span value',
+            defaultValue: 'HTML'
+        }
+    },
+    args: {
+        title: 'HTML'
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof EditableSpan>;
+
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+export const EditableSpanStory: Story = {}
