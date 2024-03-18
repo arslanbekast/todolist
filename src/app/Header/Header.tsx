9c5cf63095282e21096597fffc66f4662b80fb7d
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-03-18 21:03:58 +0300

    Fix Header component

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { AppBar, Button, IconButton, LinearProgress, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Button, IconButton, LinearProgress, Toolbar, Typography } from "@mui/material"
 import { Menu } from "@mui/icons-material"
 import { useSelector } from "react-redux"
 import { selectAppStatus } from "app/app.selectors"
@@ -16,19 +16,23 @@ export const Header = () => {
     const logoutHandler = () => logout()
 
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <Menu />
-                </IconButton>
-                <Typography variant="h6">News</Typography>
-                {isLoggedIn && (
-                    <Button color="inherit" onClick={logoutHandler}>
-                        Log out
-                    </Button>
-                )}
-            </Toolbar>
-            {status === "loading" && <LinearProgress />}
-        </AppBar>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    {isLoggedIn && (
+                        <Button color="inherit" onClick={logoutHandler}>
+                            Log out
+                        </Button>
+                    )}
+                </Toolbar>
+                {status === "loading" && <LinearProgress />}
+            </AppBar>
+        </Box>
     )
 }
