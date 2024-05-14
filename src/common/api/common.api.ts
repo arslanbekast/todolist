cb570046b3ec29fc916962c7752a0b84fba97fb2
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-05-14 21:15:57 +0300

    Change API-KEY in common.api.ts

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.1/",
     withCredentials: true,
     headers: {
-        "API-KEY": "02589a7c-e3a4-4241-b4b5-ffbc85a6aec7",
+        "API-KEY": "bd90b833-7e9a-4cbd-b628-b221fb8d5c21",
     },
 })
